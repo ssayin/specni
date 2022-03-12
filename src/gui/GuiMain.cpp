1874e73355cae439f372c6da421083f92c14b925
Serdar Sayın <nefer.kha.ptah@gmail.com>
2022-03-13 02:18:58 +0300

    enable docking

--- src/gui/GuiMain.cpp
+++ src/gui/GuiMain.cpp
@@ -77,6 +77,9 @@ int gui(int, char **) {
 	ImGui::CreateContext();
 	ImGuiIO &io = ImGui::GetIO();
 	(void)io;
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
+
 	// io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable
 	// Keyboard Controls io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad; //
 	// Enable Gamepad Controls
